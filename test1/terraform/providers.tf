1eddb380dcb0f4af44a2aa7c2b9ec012c63aa8cc
Erik Ahrend <kire317@gmail.com>
2019-01-19 15:04:35 -0500

    removing credentials

--- test1/terraform/providers.tf
+++ test1/terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAI226GILFLNWVPIBA"
-  secret_key = "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"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key"
   region     = "us-east-1"
 }
